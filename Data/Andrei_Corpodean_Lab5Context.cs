0ec9a109dcd1f18ed6b1a4666ed14281194166a6
Andrei Corpodean <andrei.corpodean@gmail.com>
2024-11-26 19:26:32 +0200

    Add project files.

---
+++ Data/Andrei_Corpodean_Lab5Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Andrei_Corpodean_Lab5.Models;
+
+namespace Andrei_Corpodean_Lab5.Data
+{
+    public class Andrei_Corpodean_Lab5Context : DbContext
+    {
+        public Andrei_Corpodean_Lab5Context (DbContextOptions<Andrei_Corpodean_Lab5Context> options) : base(options)
+        {
+        }
+
+        public DbSet<Expense> Expense { get; set; } = default!;
+    }
+}
---
+++ Data/ExpenseContext.cs
@@ -0,0 +1,14 @@
+﻿using Andrei_Corpodean_Lab5.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Andrei_Corpodean_Lab5.Data
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options) : base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+
+    }
+}
